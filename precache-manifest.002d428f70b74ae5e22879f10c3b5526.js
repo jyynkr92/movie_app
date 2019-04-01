e645be7b2fb83dab97e7749a2a1eb705742c879a
aliceyoo <jyynkr92@gmail.com>
2019-04-01 15:46:54 +0900

    Updates

---
+++ precache-manifest.002d428f70b74ae5e22879f10c3b5526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "4a44f3c325f33a26bf05",
+    "url": "/movie_app/static/js/main.0063c32c.chunk.js"
+  },
+  {
+    "revision": "a9df4098280d736e03c5",
+    "url": "/movie_app/static/js/2.f092342e.chunk.js"
+  },
+  {
+    "revision": "4a44f3c325f33a26bf05",
+    "url": "/movie_app/static/css/main.c9f3de8f.chunk.css"
+  },
+  {
+    "revision": "43c83a8a17f7147a71171535f5c26514",
+    "url": "/movie_app/index.html"
+  }
+];
